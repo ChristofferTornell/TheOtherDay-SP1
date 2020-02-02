d62ee836719cd61aa29be227db00f99d35de48b2
Jens Bakklund <je2ba@outlook.com>
2020-02-02 20:36:43 +0100

    Started on the PlayerMovement Script
    Added and started on the PlayerMovement Script, Added a Player Prefab as a placeholder to test the script on. Changed the game name to "The Other Day"

---
+++ TheOtherDay-SP1/Assets/Content/Player/PlayerMovement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [Header("Stats")]
+    [Tooltip("Movement speed of the player (recommended value between 350 - 500)")]
+    [SerializeField] private float movementSpeed = 450;
+
+    private Rigidbody2D rb = null;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();   
+    }
+
+    void PlayerInput()
+    {
+        rb.velocity = Vector2.zero;
+
+        // Right
+        if (Input.GetAxisRaw("Horizontal") > 0)
+        {
+            rb.velocity = Vector2.right * movementSpeed * Time.deltaTime;
+        }
+
+        // Left
+        if (Input.GetAxisRaw("Horizontal") < 0)
+        {
+            rb.velocity = Vector2.left * movementSpeed * Time.deltaTime;
+        }
+    }
+
+    void Update()
+    {
+        PlayerInput();
+    }
+}

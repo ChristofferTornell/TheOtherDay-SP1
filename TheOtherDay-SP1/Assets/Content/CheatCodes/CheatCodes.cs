a076468febcefd1385968a19566eb771bd4c1c95
Christoffer Tornell <christoffer.tornell@gmail.com>
2020-02-03 14:52:22 +0100

    Auto stash before merge of "master" and "origin/master"

---
+++ TheOtherDay-SP1/Assets/Content/CheatCodes/CheatCodes.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheatCodes : MonoBehaviour
+{
+    [Header("Hangover")]
+    public HangoverManager hangoverManager;
+    public int increaseHangoverAmount = 1;
+    public int decreaseHangoverAmount = 1;
+
+    void Start()
+    {
+
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.H)) //TESTING PURPOSE
+        {
+            hangoverManager.CurrentHangover += increaseHangoverAmount;
+        }
+        if (Input.GetKeyDown(KeyCode.J)) //TESTING PURPOSE
+        {
+            hangoverManager.CurrentHangover -= decreaseHangoverAmount;
+        }
+    }
+}
---
+++ TheOtherDay-SP1/Assets/Content/Player/Hangover/HangoverManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[CreateAssetMenu(fileName = "HangoverData", menuName = "ScriptableObjects/HangoverManager", order = 1)]
+
+public class HangoverManager : ScriptableObject
+{
+    public UnityAction<HangoverManager, int> onChange = delegate { };
+    public int startHangover = 100;
+    private int _currentHangover = 0;
+    public int CurrentHangover
+    {
+        get
+        {
+            return _currentHangover;
+        }
+
+        set
+        {
+            if (_currentHangover != value)
+            {
+                int hangoverChange = value - _currentHangover;
+                _currentHangover = value;
+                onChange(this, hangoverChange);
+            }
+        }
+    }
+    public void OnEnable()
+    {
+        _currentHangover = startHangover;
+    }
+
+}

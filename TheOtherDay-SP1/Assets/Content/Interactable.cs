6114163619e782817775f869d2ae9fb1e6b8dfe5
Jens Bakklund <je2ba@outlook.com>
2020-02-03 11:51:26 +0100

    Tweaked a little in PlayerInteractivity

--- TheOtherDay-SP1/Assets/Content/Interactable.cs
+++ TheOtherDay-SP1/Assets/Content/Interactable.cs
@@ -1,8 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
+
+[System.Serializable]
+public class InteractEvent : UnityEvent<string, GameObject> { }
 
 public class Interactable : MonoBehaviour
 {
+
+    public InteractEvent OnInteract;
     // What to have here?
+
+    // On interact, do a list of events
+    // Ability to add events in inspcetor
+
+    public void Interact()
+    {
+
+    }
+
+    public void TestEvent()
+    {
+
+    }
 }

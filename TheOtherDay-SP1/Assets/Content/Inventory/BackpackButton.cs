37c2bd5c1c41f17d08f3756d3261e4ce7973c266
Noah Silveira <noah.silveira@outlook.com>
2020-02-04 15:42:54 +0100

    Inventory prototype

---
+++ TheOtherDay-SP1/Assets/Content/Inventory/BackpackButton.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BackpackButton : MonoBehaviour
+{
+    public Button button;
+    private bool inventory = false;
+    public GameObject hotbar;
+
+    private void Start()
+    {
+        button.onClick.AddListener(OnOff);
+    }
+
+    void OnOff()
+    {
+        if(inventory == false)
+        {
+            hotbar.SetActive(true);
+            inventory = true;
+        }
+        else
+        {
+            hotbar.SetActive(false);
+            inventory = false;
+        }
+    }
+}

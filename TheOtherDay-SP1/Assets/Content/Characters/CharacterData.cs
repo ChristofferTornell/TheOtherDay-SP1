da3a40df24bfe6a57849b78c9078f8040654c57b
Noah Silveira <noah.silveira@outlook.com>
2020-02-04 12:10:48 +0100

    Scriptable object characters

---
+++ TheOtherDay-SP1/Assets/Content/Characters/CharacterData.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Character", menuName = "Character")]
+public class CharacterData : ScriptableObject
+{
+    public new string name;
+    public Sprite contactImage;
+    public Sprite dialogImage;
+    public Font font;
+    //public textsound
+    //public textcolor
+
+}
---
+++ TheOtherDay-SP1/Assets/Content/PhoneTest/MessageSquare.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MessageSquare : MonoBehaviour
+{
+    public CharacterData character;
+    public Image image;
+    public TextMeshProUGUI text;
+    
+    void Start()
+    {
+        image.sprite = character.contactImage;
+        text.text = character.name;
+    }
+}

fb9f3655a8c15e184bafc6bcae770136d9194017
Christoffer Tornell <christoffer.tornell@gmail.com>
2020-02-04 13:36:28 +0100

    Added dialogueSystem scripts

---
+++ TheOtherDay-SP1/Assets/Content/DialogueSystem/Dialogue.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class Dialogue : MonoBehaviour
+{
+    //public CharacterProfile character;
+    public enum CharacterEmotion
+    {
+        normal,
+        happy,
+        sad
+    };
+
+    public CharacterEmotion characterEmotion = CharacterEmotion.normal;
+
+    public TextMeshProUGUI textObject;
+
+    [TextArea(15, 20)]
+    public string dialog = "";
+
+    //public ChoiceButton[] choiceButtons = null;
+    public Button nextButtonObject = null;
+    public Dialogue nextDialogue = null;
+}

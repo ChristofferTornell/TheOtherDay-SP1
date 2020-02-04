8383aef9a6d6ad8893b016ff05d1ccf718e18b6e
Jens Bakklund <je2ba@outlook.com>
2020-02-04 16:01:56 +0100

    Started working on CameraFollowPlayer Script

---
+++ TheOtherDay-SP1/Assets/Content/Player/CameraFollowPlayer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowPlayer : MonoBehaviour
+{
+    public GameObject player;
+    public float xOffset;
+
+    // Camera moves with player
+    // If player reaches edge. Camera should stop as if colliding with the edge
+    // Player should be "pushing" the camera around. A dead zone in the middle were the camera doesnt follow player.
+    // Inspiration: Super Mario: World https://www.youtube.com/watch?v=TCIMPYM0AQg
+
+    void Start()
+    {
+        // Place the camera on the player with offset
+        gameObject.transform.position = new Vector3(player.transform.position.x + xOffset, transform.position.y, -10);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.transform.position = new Vector3(player.transform.position.x + xOffset, transform.position.y, -10);
+    }
+}

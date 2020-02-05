8920edaa093e2959471df92a4edff992b65d169a
Jens Bakklund <je2ba@outlook.com>
2020-02-05 14:33:40 +0100

    Downloaded Quick Search, 2D Sprite and 2D Tile editor packages
    As well as some minor script and scene changes

--- TheOtherDay-SP1/Assets/Content/LightMultiplier.cs
+++ TheOtherDay-SP1/Assets/Content/LightMultiplier.cs
@@ -1,13 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.Rendering;
 using UnityEngine.Experimental.Rendering.LWRP;
 
 public class LightMultiplier : MonoBehaviour
 {
-    public static float lightIntensityMultiplier = 1f;
+    // Easy access to changing the light multiplier from other classes
+    // On changing the values. The LightMultiplier Script on all lights takes the new change
+
+    public UnityAction<float> onLightChange = delegate { };
+
     Light2D light2D = null;
+    public static float lightIntensityMultiplier = 1;
+
+    public void ChangeLight(float amount)
+    {
+        lightIntensityMultiplier += amount;
+        light2D.intensity = light2D.intensity * lightIntensityMultiplier;
+    }
 
     private void Start()
     {
@@ -17,10 +29,5 @@ public class LightMultiplier : MonoBehaviour
 
             light2D.intensity = light2D.intensity * lightIntensityMultiplier;
         }
-    }
-
-    private void Update()
-    {
-        //light2D.intensity = light2D.intensity * lightIntensityMultiplier;
     }
 }

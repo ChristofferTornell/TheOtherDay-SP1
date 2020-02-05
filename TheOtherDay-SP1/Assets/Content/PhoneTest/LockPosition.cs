58497a80acf56864f2ec58065db4183fa9f345fc
Noah Silveira <noah.silveira@outlook.com>
2020-02-05 12:26:42 +0100

    Scrolling system for photos

---
+++ TheOtherDay-SP1/Assets/Content/PhoneTest/LockPosition.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LockPosition : MonoBehaviour
+{
+    private float x;
+    private float y;
+
+    void Start()
+    {
+        x = GetComponentInParent<Scroll>().initX;
+        y = GetComponentInParent<Scroll>().initY;
+    }
+
+    void Update()
+    {
+        transform.position = new Vector3(x, y, 0);
+    }
+}
---
+++ TheOtherDay-SP1/Assets/Content/PhoneTest/Scroll.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Scroll : MonoBehaviour
+{
+    public Rigidbody2D body;
+    [HideInInspector]
+    public float initX;
+    [HideInInspector]
+    public float initY;
+
+    private void Start()
+    {
+        initX = transform.position.x;
+        initY = transform.position.y;
+    }
+    void Update()
+    {
+        body.velocity = new Vector3(0, Input.GetAxis("Mouse ScrollWheel") * Mathf.Pow(10, 4), 0);
+    }
+}

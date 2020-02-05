3509124df33cb487b831bc66b036408b311314c4
Jens Bakklund <je2ba@outlook.com>
2020-02-05 12:12:45 +0100

    Played around with 2D Lighting and started on HangoverLightEffect Script

---
+++ TheOtherDay-SP1/Assets/Content/Player/Hangover/HangoverLightEffect.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HangoverLightEffect : MonoBehaviour
+{
+    public void IncreaseLight(float amount)
+    {
+        LightMultiplier.lightIntensityMultiplier += amount;
+    }
+
+    public void DecreaseLight(float amount)
+    {
+        LightMultiplier.lightIntensityMultiplier -= amount;
+    }
+
+    public IEnumerator lightExposureEffect(float difference, float lightIncreaseTime, float stabilizationTime)
+    {
+        // Increase lightMultiplier by difference / time. Then stabilize the light multiplier to old amount over time. 
+        yield return null;
+    }
+}

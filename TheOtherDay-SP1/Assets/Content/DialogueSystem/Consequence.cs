eb1ab937439721d24ca3c97f5c87b2c50ed74796
Jens Bakklund <je2ba@outlook.com>
2020-02-10 14:11:53 +0100

    Added some assets, added new DDOL Script and tweaked in other scripts
    DDOL = DontDestroyOnLoad to be used in scene transfer. Need to work on removing duplicates

---
+++ TheOtherDay-SP1/Assets/Content/DialogueSystem/Consequence.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Consequence : MonoBehaviour
+{
+    private static int currentReputation = 0;
+
+    public void REP_ChangeReputation(int difference)
+    {
+        currentReputation += difference;
+    }
+
+    public static int GetCurrentReputation()
+    {
+        return currentReputation;
+    }
+}

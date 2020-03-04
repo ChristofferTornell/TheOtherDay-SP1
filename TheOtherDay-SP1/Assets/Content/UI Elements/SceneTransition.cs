d7eac4a6156463bfaed63a7d0147e2d7501bb4ce
Jens Bakklund <je2ba@outlook.com>
2020-03-04 15:07:26 +0100

    Started on SceneTransitions

---
+++ TheOtherDay-SP1/Assets/Content/UI Elements/SceneTransition.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SceneTransition : MonoBehaviour
+{
+    public Image panelImage;
+    public RectTransform imagePosition = null;
+    [Header("Transition Stuf")]
+    public Animator animator = null;
+    public AnimationClip fadeInAnimation = null;
+    public Color fadeInColor;
+    public AnimationClip fadeOutAnimation = null;
+    public Color fadeOutColor;
+    public Color transitionColor;
+
+    private void Start()
+    {
+        SceneManager.sceneUnloaded += OnSceneUnloaded; 
+        SceneManager.sceneLoaded += OnSceneLoaded; 
+    }
+
+    public void TRAN_FadeIn()
+    {
+        panelImage.color = fadeInColor;
+        animator.Play(fadeInAnimation.name);
+    }
+
+    public void TRAN_FadeOut()
+    {
+        panelImage.color = fadeOutColor;
+        animator.Play(fadeOutAnimation.name);
+    }
+
+    public void OnSceneUnloaded(Scene scene)
+    {
+        Debug.Log(gameObject.scene.name + " unloaded");
+    }
+
+    public void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        animator.Play(fadeOutAnimation.name);
+        Debug.Log(scene.name + " loaded");
+    }
+}

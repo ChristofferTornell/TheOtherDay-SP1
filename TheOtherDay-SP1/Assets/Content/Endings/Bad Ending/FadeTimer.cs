0a290aad8957b2fb89f2d45523ffdc278d0e8b17
Noah Silveira <noah.silveira@outlook.com>
2020-03-11 16:23:46 +0100

    Bad ending draft

---
+++ TheOtherDay-SP1/Assets/Content/Endings/Bad Ending/FadeTimer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeTimer : MonoBehaviour
+{
+    private float Counter = 0;
+    public Image image;
+    public float time;
+    private float Alpha = 1;
+    public float FadeRate = 0.005f;
+
+    private void Update()
+    {
+        Counter += Time.deltaTime;
+        if (Counter > time)
+        {
+            Alpha -= FadeRate;
+            image.color = new Color(1, 1, 1, Alpha);
+        }
+    }
+}

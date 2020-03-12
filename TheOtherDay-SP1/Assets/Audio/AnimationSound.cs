9e0b9f075b08072402dae2fd30e6676f1f617125
Christoffer Tornell <christoffer.tornell@gmail.com>
2020-03-12 17:34:10 +0100

    Learned how to add sound to animation keyframe (placeholder vomit sound)

---
+++ TheOtherDay-SP1/Assets/Audio/AnimationSound.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class AnimationSound : MonoBehaviour
+{
+    [FMODUnity.EventRef] public string rileyVomitSound;
+    public void PlayVomitSound()
+    {
+        FMODUnity.RuntimeManager.PlayOneShot(rileyVomitSound);
+    }
+}
+

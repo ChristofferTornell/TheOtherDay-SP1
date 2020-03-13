e4a3faf50d047078f1f119e0c3853e7737c0b8e1
Noah Silveira <noah.silveira@outlook.com>
2020-03-13 16:25:46 +0100

    Endings

---
+++ TheOtherDay-SP1/Assets/Content/Endings/FadeTimer.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class FadeTimer : MonoBehaviour
+{
+    private float Counter = 0;
+    public Image image;
+    public TextMeshProUGUI text;
+    public float time;
+    private float Alpha = 1;
+    public float FadeRate = 0.005f;
+
+    private void Start()
+    {
+        if(image != null)
+        {
+            Alpha = 1;
+        }
+        else if(text != null)
+        {
+            Alpha = 0;
+            text.color = new Color(1, 1, 1, 0);
+        }
+    }
+
+    private void Update()
+    {
+        Counter += Time.deltaTime;
+        if (Counter > time && image != null)
+        {
+            if(image.color.a != 0)
+            {
+                Alpha -= FadeRate;
+                image.color = new Color(1, 1, 1, Alpha);
+            }
+        }
+        else if (Counter > time && text != null)
+        {
+            if(text.color.a != 1)
+            {
+                Alpha += FadeRate;
+                text.color = new Color(1, 1, 1, Alpha);
+            }
+        }
+    }
+}

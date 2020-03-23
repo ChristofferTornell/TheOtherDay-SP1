5afcdfcfdcc6bb038fd60d40bfaa8c7640b1f6b6
Noah <noah.silveira@outlook.com>
2020-03-23 14:39:25 +0100

    Intro cinematic in a scene

---
+++ TheOtherDay-SP1/Assets/IntroCinematic.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class IntroCinematic : MonoBehaviour
+{
+    public Sprite[] sprite;
+    public float[] duration;
+    private Image img;
+
+    private void Start()
+    {
+        img = gameObject.GetComponent<Image>();
+        StartCoroutine(Intro());
+    }
+
+    IEnumerator Intro()
+    {
+        for (int i = 0; i < sprite.Length; i++)
+        {
+            yield return new WaitForSeconds(duration[i]);
+            if(i < sprite.Length - 1)
+            {
+                img.sprite = sprite[i + 1];
+            }
+        }
+    }
+}

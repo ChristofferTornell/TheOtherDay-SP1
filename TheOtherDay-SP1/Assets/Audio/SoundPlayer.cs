6d086fd32e2562b43765dd0fd1bf07a0c8c80e79
Christoffer Tornell <christoffer.tornell@gmail.com>
2020-03-24 18:02:06 +0100

    More phone button sounds (needs more bugfixing)

---
+++ TheOtherDay-SP1/Assets/Audio/SoundPlayer.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundPlayer : MonoBehaviour
+{
+    [FMODUnity.EventRef] public string soundEvent;
+
+    public void PlayAudio()
+    {
+        FMODUnity.RuntimeManager.PlayOneShot(soundEvent);
+    }
+}

1b0e6a908e0622b0ef70dbe756d0309e7c977e05
Christoffer Tornell <christoffer.tornell@gmail.com>
2020-03-25 19:11:25 +0100

    fixed animation sounds, made some of them 3d

--- TheOtherDay-SP1/Assets/Audio/SoundPlayer.cs
+++ TheOtherDay-SP1/Assets/Audio/SoundPlayer.cs
@@ -5,9 +5,27 @@ using UnityEngine;
 public class SoundPlayer : MonoBehaviour
 {
     [FMODUnity.EventRef] public string soundEvent;
+    FMOD.Studio.EventInstance soundEventInstance;
+    private bool is3DSound;
 
     public void PlayAudio()
     {
         FMODUnity.RuntimeManager.PlayOneShot(soundEvent);
+    }
+    public void PlayAudio3D()
+    {
+        is3DSound = true;
+        soundEventInstance.start();
+    }
+    private void Start()
+    {
+        soundEventInstance = FMODUnity.RuntimeManager.CreateInstance(soundEvent);
+    }
+    private void Update()
+    {
+        if(is3DSound)
+        {
+            FMODUnity.RuntimeManager.AttachInstanceToGameObject(soundEventInstance, GetComponent<Transform>(), GetComponent<Rigidbody>());
+        }
     }
 }

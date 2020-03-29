19332cec8804065ddc139aada43a76a4e899001b
Jens Bakklund <je2ba@outlook.com>
2020-03-29 13:54:21 +0200

    Fixed vomit animation and spawning of vomit
    *yuck*

--- TheOtherDay-SP1/Assets/AnimationEvent.cs
+++ TheOtherDay-SP1/Assets/AnimationEvent.cs
@@ -5,14 +5,20 @@ using UnityEngine.Events;
 
 public class AnimationEvent : MonoBehaviour
 {
-    public GameObject vomitSprite = null;
+    public Sprite vomitSprite = null;
     public void PrintEvent()
     {
         Debug.Log("PrintEvent");
     }
 
-    public void InstantiateVomit()
+    public void SpawnVomit()
     {
-        GameObject vomit = Instantiate(vomitSprite, new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, gameObject.transform.position.z * 0), Quaternion.identity);        
+        GameObject vomit = FindObjectOfType<VomitScript>().gameObject;
+        vomit.GetComponent<SpriteRenderer>().sprite = vomitSprite;
     }
+
+    //public void InstantiateVomit()
+    //{
+    //    GameObject vomit = Instantiate(vomitSprite, new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, gameObject.transform.position.z * 0), Quaternion.identity);        
+    //}
 }
---
+++ TheOtherDay-SP1/Assets/Content/Player/VomitScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VomitScript : MonoBehaviour
+{
+    private SpriteRenderer spriteRenderer = null;
+
+    private void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer.sprite = null;
+    }
+}
